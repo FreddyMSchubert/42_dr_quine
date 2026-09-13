9e4baec16611c09a64682ce895fa3e850c86065d
FreddyMSchubert <github@frederickschubert.de>
2026-09-13 17:21:37 +0200

    Grace

--- C/Colleen.c
+++ C/Colleen.c
@@ -4,12 +4,13 @@
 
 void another_function_in_addition_to_main(void)
 {
-	char *content = "#include <stdio.h>%c%c// comment outside of main%c%cvoid another_function_in_addition_to_main(void)%c{%c%cchar *content = %c%s%c;%c%cprintf(content, 10, 10, 10, 10, 10, 10, 9, 34, content, 34, 10, 9, 10, 10, 10, 10, 10, 9, 10, 9, 10, 10);%c}%c%cint main(void)%c{%c%c// Wie heisst der Bürgermeister von Wesel? (Esel)%c%canother_function_in_addition_to_main();%c}%c";
-	printf(content, 10, 10, 10, 10, 10, 10, 9, 34, content, 34, 10, 9, 10, 10, 10, 10, 10, 9, 10, 9, 10, 10);
+	char *content = "#include <stdio.h>%c%c// comment outside of main%c%cvoid another_function_in_addition_to_main(void)%c{%c%cchar *content = %c%s%c;%c%cprintf(content, 10, 10, 10, 10, 10, 10, 9, 34, content, 34, 10, 9, 10, 10, 10, 10, 10, 9, 10, 9, 10, 9, 10, 10);%c}%c%cint main(void)%c{%c%c// Wie heisst der Bürgermeister von Wesel? (Esel)%c%canother_function_in_addition_to_main();%c%creturn (0);%c}%c";
+	printf(content, 10, 10, 10, 10, 10, 10, 9, 34, content, 34, 10, 9, 10, 10, 10, 10, 10, 9, 10, 9, 10, 9, 10, 10);
 }
 
 int main(void)
 {
 	// Wie heisst der Bürgermeister von Wesel? (Esel)
 	another_function_in_addition_to_main();
+	return (0);
 }
--- C/Grace.c
+++ C/Grace.c
@@ -1,3 +1,6 @@
-// TODO
-
-int main(void){}
+// todo insert something funny
+#include <stdio.h>
+#define CONTENT "// todo insert something funny%c#include <stdio.h>%c#define CONTENT %c%s%c%c#define WRITE(file) fprintf(file, CONTENT, 10, 10, 34, CONTENT, 34, 10, 10, 34, 34, 34, 34, 10, 10)%c#define MAIN() int main(void) { FILE *file = fopen(%cGrace_kid.s%c, %cw%c); if (!file) return 1; WRITE(file); fclose(file); return 0; }%cMAIN()%c"
+#define WRITE(file) fprintf(file, CONTENT, 10, 10, 34, CONTENT, 34, 10, 10, 34, 34, 34, 34, 10, 10)
+#define MAIN() int main(void) { FILE *file = fopen("Grace_kid.s", "w"); if (!file) return 1; WRITE(file); fclose(file); return 0; }
+MAIN()
